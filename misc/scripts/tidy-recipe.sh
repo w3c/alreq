e67110d299b568c2a540cdf39573c8cf1315ed06
Behnam Esfahbod <behnam@behnam.es>
2017-04-18 23:08:26 -0500

    Update tidy-recipe.sh and apply it

--- misc/scripts/tidy-recipe.sh
+++ misc/scripts/tidy-recipe.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 
-tidy -utf8 --wrap 0  --tidy-mark false --output-html true -i --new-blocklevel-tags section,figure,figcaption -m index.html
+HTML_FILES='index.html'
+
+tidy -utf8 \
+  --break-before-br yes \
+  --indent auto \
+  --new-blocklevel-tags section,figure,figcaption \
+  --output-html yes \
+  --quiet yes \
+  --tidy-mark no \
+  --vertical-space yes \
+  --wrap 100 \
+  -m $HTML_FILES
+
